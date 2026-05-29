b77d16cc4abd0445fdb0b3a2bfc1aa3c3e2052a1
REGS <stevenregolino@gmail.com>
2026-05-29 19:03:22 -0400

    touch ups

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     "g_keys": {
-        "G1": { "label": "Consolidate Selection", "cmd": "Ctrl+Alt+C" },
+        "G1": { "label": "Open Hotkey Page", "cmd": "System Script" },
         "G2": { "label": "Consolidate Selection", "cmd": "Ctrl+Alt+C" },
         "G3": { "label": "Render Armed Tracks", "cmd": "Alt+R" },
         "G4": { "label": "Quick Route to Mixer", "cmd": "Ctrl+L" },
@@ -17,10 +17,10 @@ const CONFIG = {
         { "id": "m_wheel_l", "label": "Wheel Left", "action": "Next Pattern" },
         { "id": "m_wheel_r", "label": "Wheel Right", "action": "Previous Pattern" },
         { "id": "m_thumb_1", "label": "G8 Button", "action": "Toggle metronome (Ctrl+M)" },
-        { "id": "m_thumb_2", "label": "Thumb Forward", "action": "Save New Version (Ctrl+N)" },
-        { "id": "m_thumb_snipe", "label": "Thumb Snipe", "action": "Stop sound (Ctrl+H)" },
-        { "id": "m_side_1", "label": "Index Side Top", "action": "Close All Windows (F12)" },
-        { "id": "m_side_2", "label": "Index Side Mid", "action": "REDO/QUICK UNDO (Ctrl+Alt+Z)" },
-        { "id": "m_side_3", "label": "Index Side Bot", "action": "UNDO (Ctrl+Z)" }
+        { "id": "m_thumb_2", "label": "G7 Button", "action": "Save New Version (Ctrl+N)" },
+        { "id": "m_thumb_snipe", "label": "G9 Small Button", "action": "Stop sound (Ctrl+H)" },
+        { "id": "m_side_1", "label": "Thumb Large", "action": "Close All Windows (F12)" },
+        { "id": "m_side_2", "label": "Thumb Forward", "action": "REDO/QUICK UNDO (Ctrl+Alt+Z)" },
+        { "id": "m_side_3", "label": "Thumb Back", "action": "UNDO (Ctrl+Z)" }
     ]
 };
